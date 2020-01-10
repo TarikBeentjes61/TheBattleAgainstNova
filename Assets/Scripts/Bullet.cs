4d018b53e9820aa473462703e4eac163cf532680
TarikBeentjes61 <50704992+TarikBeentjes61@users.noreply.github.com>
2020-01-10 15:32:47 +0100

    Update
    Begin fase van de Enemy. Wat sprites toegevoegt

---
+++ Assets/Scripts/Bullet.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Bullet : MonoBehaviour {
+    public Rigidbody2D rb;
+    public int Damage;
+    public GameObject Explosion;
+    public float Speed;
+    
+    void Start() {
+        rb.velocity = transform.up * Speed;
+    }
+
+    private void OnCollisionEnter2D(Collision2D other) {
+        if (!other.transform.CompareTag("Enemy")) {
+            Instantiate(Explosion, other.GetContact(0).point, Quaternion.identity);
+            Destroy(gameObject);
+        }
+        if (other.transform.CompareTag("Player")) {
+            //other.transform.GetComponent<ScriptWithHealth>.Health - Damage;
+        }
+
+        
+    }
+}
---
+++ Assets/Scripts/Enemy.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEditor;
+using UnityEngine;
+
+public class Enemy : MonoBehaviour
+{
+    public float MoveTime;
+    public GameObject Player;
+    public int RoffSet;
+    public GameObject Bullet;
+    public GameObject ShootingPoint;
+    public float ShootCooldown;
+    private float _cooldownTimer;
+    
+    void Update() {
+        var angle = (Player.transform.position - transform.position);    
+        var calc = Mathf.Atan2 ( angle.y, angle.x );
+        transform.rotation = Quaternion.Euler(0f, 0f, calc * Mathf.Rad2Deg - RoffSet);
+        Move(calc);
+        _cooldownTimer += Time.deltaTime;
+        if (!(_cooldownTimer > ShootCooldown)) return;
+        Shoot(calc);
+        _cooldownTimer = 0;
+    }
+
+    private void Shoot(float calc) {
+        
+        GameObject clone = Bullet;
+        Instantiate(clone, ShootingPoint.transform.position, Quaternion.Euler(0f, 0f, calc * Mathf.Rad2Deg - RoffSet));
+        clone.transform.GetComponent<Rigidbody2D>().velocity = transform.right * MoveTime;
+    }
+    
+    private void Move(float calc) {
+        float singleStep = MoveTime * Time.deltaTime;
+        transform.position = Vector2.Lerp(transform.position, Player.transform.position, singleStep);
+        transform.rotation = Quaternion.Euler(0f, 0f, calc * Mathf.Rad2Deg - RoffSet);
+    }
+}
