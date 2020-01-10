e7d396a3806119a162163f7ba7d066e5fc051c1b
TarikBeentjes61 <50704992+TarikBeentjes61@users.noreply.github.com>
2020-01-10 15:53:59 +0100

    Update

--- Assets/Scripts/Bullet.cs
+++ Assets/Scripts/Bullet.cs
@@ -6,21 +6,25 @@ public class Bullet : MonoBehaviour {
     public Rigidbody2D rb;
     public int Damage;
     public GameObject Explosion;
+    private GameObject _explosionToDestroy;
     public float Speed;
     
     void Start() {
         rb.velocity = transform.up * Speed;
     }
 
+    private void Update() {
+        Destroy(_explosionToDestroy, 1f);
+    }
+
     private void OnCollisionEnter2D(Collision2D other) {
         if (!other.transform.CompareTag("Enemy")) {
-            Instantiate(Explosion, other.GetContact(0).point, Quaternion.identity);
+            _explosionToDestroy = Explosion;
+            Instantiate(_explosionToDestroy, other.GetContact(0).point, Quaternion.identity);
             Destroy(gameObject);
         }
         if (other.transform.CompareTag("Player")) {
             //other.transform.GetComponent<ScriptWithHealth>.Health - Damage;
         }
-
-        
     }
 }
