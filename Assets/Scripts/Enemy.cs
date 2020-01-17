46317aba2f3a5562a0e9536a722ad35c4afc29e7
TarikBeentjes61 <50704992+TarikBeentjes61@users.noreply.github.com>
2020-01-17 15:24:03 +0100

    Update
    Licht systeem ingevoegd. Enemies geupdate. Nieuwe sprites en achtergrond.

--- Assets/Scripts/Enemy.cs
+++ Assets/Scripts/Enemy.cs
@@ -9,6 +9,7 @@ public class Enemy : MonoBehaviour
 {
     [Header("Variables")]
     public float MoveTime;
+    public float DistanceToKeep;
     public int BurstSpread; //Hoe ver de kogels verspreiden
     public int BurstAmount; //Aantal kogels in een burst
     public float TimeBetweenMoves;
@@ -21,30 +22,61 @@ public class Enemy : MonoBehaviour
     public GameObject ShootingPoint;
     private float _cooldownTimer;
     private float _moveTimer;
+    private bool _moveCompleted;
+
+    private Vector3 _angle;
+    private float _calc;
 
     private void Start() {
         _moveTimer = TimeBetweenMoves;
+        _moveCompleted = true;    
     }
 
     private void Update() {
         _moveTimer += Time.deltaTime;
-        if (_moveTimer > TimeBetweenMoves) {
+        if (_moveCompleted) {
+            _moveCompleted = false;
+            Random random = new Random();
+            int randomNumber = random.Next(0, 1);
+            if (randomNumber == 0) {
+                if (Vector3.Distance(transform.position, Player.transform.position) > DistanceToKeep * 1.5f) {
+                    StartCoroutine(Move());
+                }
+                else {
+                    randomNumber = 1;
+                }
+            }
+            if (randomNumber == 1) {
+            }
             _moveTimer = 0;
         }
-        var angle = (Player.transform.position - transform.position);    
-        var calc = Mathf.Atan2 ( angle.y, angle.x );
-        transform.rotation = Quaternion.Euler(0f, 0f, calc * Mathf.Rad2Deg - RoffSet);
+        _angle = Player.transform.position - transform.position;    
+        _calc = Mathf.Atan2(_angle.y, _angle.x);
+        transform.rotation = Quaternion.Euler(0f, 0f, _calc * Mathf.Rad2Deg - RoffSet);
         _cooldownTimer += Time.deltaTime;
         if (_cooldownTimer > ShootCooldown) {
-            StartCoroutine(Burst());
+            Shoot();
             _cooldownTimer = 0;
         }
     }
 
-    private void Shoot(float calc) {
+    private void Shoot() {
         GameObject clone = Bullet;
-        Instantiate(clone, ShootingPoint.transform.position, Quaternion.Euler(0f, 0f, calc * Mathf.Rad2Deg - RoffSet));
+        Instantiate(clone, ShootingPoint.transform.position, Quaternion.Euler(0f, 0f, _calc * Mathf.Rad2Deg - RoffSet));
         clone.transform.GetComponent<Rigidbody2D>().velocity = transform.right * MoveTime;
+        _moveCompleted = true;
+    }
+
+    private IEnumerator Move() {
+        while (Vector3.Distance(transform.position, Player.transform.position) > DistanceToKeep) {
+            transform.position =
+                Vector3.MoveTowards(transform.position, Player.transform.position, MoveTime * Time.deltaTime);
+            yield return null;
+        }
+
+        _moveCompleted = true;
+
+        yield return 0;
     }
 
     private IEnumerator Burst() {
---
+++ Assets/Scripts/Explosion.cs
@@ -0,0 +1,9 @@
+﻿using UnityEngine;
+
+public class Explosion : MonoBehaviour
+{
+    void Update()
+    {
+        Destroy(gameObject, 0.5f);        
+    }
+}
