6e7f99d43478024058fa5b0d974bbeb85d92f555
TarikBeentjes61 <50704992+TarikBeentjes61@users.noreply.github.com>
2020-02-14 15:10:05 +0100

    Interface / New enemy type

---
+++ Assets/Scripts/Menu.cs
@@ -0,0 +1,13 @@
+﻿using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class Menu : MonoBehaviour
+{
+    public void LoadScene(string sceneName) {
+        SceneManager.LoadScene(sceneName);
+    }
+
+    public void Quit() {
+        Application.Quit();
+    }
+}
